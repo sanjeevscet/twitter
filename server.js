2dd003f75bc08c21a5ddc68bd6716ba260ff424c
Sanjeev Bw <sanjeev@busywizzy.com>
2018-07-11 23:56:03 +0530

    Vid 22 completed

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const mongoose =require('mongoose');
+const hbs = require('hbs');
+const expressHbs = require('express-handlebars');
+
+const app = express();
+
+app.engine('.hbs', expressHbs({ defaultLayout: 'layout', extname: '.hbs' }));
+app.set('view engine', 'hbs');
+app.use(express.static(__dirname+'/public'));
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.listen(3030, (err) => {
+	if(err) {
+		console.log(err);
+	} else {
+		console.log(`Running on port ${3030}`);
+	}
+
+})
