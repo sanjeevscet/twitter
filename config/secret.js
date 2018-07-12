7c5afe0600df707fc967c7122c530c40817cc1b7
Sanjev Kumar <skumar01@goavega.com>
2018-07-12 23:51:05 +0530

    Vid 29 completed

--- config/secret.js
+++ config/secret.js
@@ -1,3 +1,4 @@
 module.exports = {
 	database: 'mongodb://root:abc123@ds233551.mlab.com:33551/sanjeevtwitter',
+	secret: 'hahhahahah',
 }
--- server.js
+++ server.js
@@ -5,6 +5,10 @@ const mongoose =require('mongoose');
 const hbs = require('hbs');
 const expressHbs = require('express-handlebars');
 const config = require('./config/secret');
+const session = require('express-session');
+const MongoStore = require('connect-mongo')(session);
+const falsh = require('express-flash');
+
 
 const app = express();
 
@@ -22,6 +26,13 @@ app.use(express.static(__dirname+'/public'));
 app.use(morgan('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(session({
+	resave: true,
+	saveUninitialized: true,
+	secret: config.secret,
+	store: new MongoStore({ url: config.database, autoReconnect: true })
+}))
+app.use(falsh());
 
 //mongodb://<dbuser>:<dbpassword>@ds233551.mlab.com:33551/sanjeevtwitter
 const mainRoutes = require('./routes/main');
