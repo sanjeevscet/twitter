23a8621b913bda0bcf1008535e89e3411e6b6778
Sanjev Kumar <skumar01@goavega.com>
2018-07-20 00:19:12 +0530

    VID 40 completed

--- routes/main.js
+++ routes/main.js
@@ -1,9 +1,18 @@
 const router = require('express').Router();
 const User = require('../models/user');
+const Tweet = require('../models/tweet');
 
 router.get('/', (req, res, next) => {
 	if(req.user) {
-		res.render('main/home');
+
+		Tweet.find({})
+			.sort('-created')
+			.populate('owner')
+			.exec(function(err, tweets) {
+				if(err) return next(err); 
+				console.log(tweets);
+				res.render('main/home', { tweets: tweets });
+			});
 	} 
 	else {
 		res.render('main/landing');
