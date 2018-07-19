5d84d760349780e8a7a9fb549f67205650522d36
Sanjev Kumar <skumar01@goavega.com>
2018-07-19 23:59:38 +0530

    VId 40 completed

--- models/tweet.js
+++ models/tweet.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const TweetSchema = new Schema({
-	owner: { type:Schema.type.ObjectId, ref: 'User' },
+	owner: { type: Schema.Types.ObjectId, ref: 'User'},
 	content: String,
 	created: {type: Date, default: Date.now }
 })
